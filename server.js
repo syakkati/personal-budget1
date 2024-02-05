ffb28c1ca25b80cad5229daa0b7cd5f47d4304c2
Somanadh Yakkati <syakkati@uncc.edu>
2024-02-04 23:43:41 -0500

    Added server, Axios, and ChartJS and Added D3JS chart

---
+++ server.js
@@ -0,0 +1,21 @@
+// Budget API
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use('/', express.static('public'));
+
+
+const budget = require("./exercise01.json");
+console.log(budget)
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+});
